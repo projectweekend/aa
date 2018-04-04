ffa49f4ee46d66a9c394ce2b060f0c33f14c8469
Brian Hines <brian@projectweekend.net>
2018-04-04 17:09:20 -0500

    organize unit conig

--- unit/unit.py
+++ unit/unit.py
@@ -6,6 +6,21 @@ ATTACK = 'attack'
 DEFENSE = 'defense'
 COST = 'cost'
 
+UNITS = {
+    INFANTRY: {ATTACK: 1, DEFENSE: 2, COST: 3},
+    ARTILLERY: {ATTACK: 2, DEFENSE: 2, COST: 4},
+    MECHANIZED_INFANTRY: {ATTACK: 1, DEFENSE: 2, COST: 4},
+    TANK: {ATTACK: 3, DEFENSE: 3, COST: 6},
+    FIGHTER: {ATTACK: 3, DEFENSE: 4, COST: 10},
+    TACTICAL_BOMBER: {ATTACK: 3, DEFENSE: 3, COST: 11},
+    STRATEGIC_BOMBER: {ATTACK: 4, DEFENSE: 1, COST: 12},
+    AIRCRAFT_CARRIER: {ATTACK: 0, DEFENSE: 2, COST: 16},
+    BATTLESHIP: {ATTACK: 4, DEFENSE: 4, COST: 20},
+    DESTROYER: {ATTACK: 2, DEFENSE: 2, COST: 8},
+    CRUISER: {ATTACK: 3, DEFENSE: 3, COST: 12},
+    SUBMARINE: {ATTACK: 2, DEFENSE: 1, COST: 6}
+}
+
 
 class Unit:
 
@@ -35,13 +50,4 @@ class Unit:
 
 
 def unit_factory(name):
-    units = {
-        INFANTRY: {ATTACK: 1, DEFENSE: 2, COST: 3},
-        ARTILLERY: {ATTACK: 2, DEFENSE: 2, COST: 4},
-        MECHANIZED_INFANTRY: {ATTACK: 1, DEFENSE: 2, COST: 4},
-        TANK: {ATTACK: 3, DEFENSE: 3, COST: 6},
-        FIGHTER: {ATTACK: 3, DEFENSE: 4, COST: 10},
-        TACTICAL_BOMBER: {ATTACK: 3, DEFENSE: 3, COST: 11},
-        STRATEGIC_BOMBER: {ATTACK: 4, DEFENSE: 1, COST: 12}
-    }
-    return Unit(name=name, **units[name])
+    return Unit(name=name, **UNITS[name])
