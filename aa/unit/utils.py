8601ef5e8be91a88417d7693782f516f4e96d7bc
Brian Hines <brian@projectweekend.net>
2018-04-07 21:52:39 -0500

    Transports can't be taken as casualties so exclude them

--- aa/unit/utils.py
+++ aa/unit/utils.py
@@ -1,3 +1,4 @@
+from .info import TRANSPORT
 from .unit import unit_factory
 
 
@@ -5,8 +6,14 @@ def _cost_and_name(unit):
     return '{0}-{1}'.format(unit.cost, unit.name)
 
 
+def remove_excluded_units(config):
+    excluded = (TRANSPORT, )
+    return {k: v for k, v in config.items() if k not in excluded}
+
+
 def build_army(config, unit_factory=unit_factory):
     army = []
+    config = remove_excluded_units(config)
     for name, count in config.items():
         army += [unit_factory(name) for _ in range(count)]
     return sorted(army, key=_cost_and_name)[::-1]
