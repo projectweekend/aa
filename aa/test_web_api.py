ba434a283a3a310f0dd7244244783bc06d39cb53
Brian Hines <brian@projectweekend.net>
2018-04-21 09:28:36 -0500

    Test simulation route

--- aa/test_web_api.py
+++ aa/test_web_api.py
@@ -14,3 +14,19 @@ def test_get_unit_info(client):
     result = client.simulate_get('/unit-info')
     for unit in result.json[LAND]:
         assert unit == UNIT_INFO[unit['name']]
+
+
+def test_simulate_battle(client):
+    battle = {
+        'attacker': {
+            'Infantry': 5,
+            'Tank': 5
+        },
+        'defender': {
+            'Infantry': 10
+        }
+    }
+    result = client.simulate_post('/', json=battle)
+    assert isinstance(result.json['attacker'], float)
+    assert isinstance(result.json['defender'], float)
+    assert isinstance(result.json['draw'], float)
