30d55db5db11bd38e6721e32d1cff0b1227abc8d
Brian Hines <brian@projectweekend.net>
2018-04-21 09:29:47 -0500

    Use constants

--- aa/web_api/info.py
+++ aa/web_api/info.py
@@ -7,7 +7,7 @@ class UnitInfoResource:
 
     def on_get(self, req, res):
         res.media = {
-            'land': [UNIT_INFO[name] for name in LAND_UNITS],
-            'air': [UNIT_INFO[name] for name in AIR_UNITS],
-            'sea': [UNIT_INFO[name] for name in SEA_UNITS]
+            LAND: [UNIT_INFO[name] for name in LAND_UNITS],
+            AIR: [UNIT_INFO[name] for name in AIR_UNITS],
+            SEA: [UNIT_INFO[name] for name in SEA_UNITS]
         }
