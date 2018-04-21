ceb31067d471456d5ce37fad3dae4a0db9c204d6
Brian Hines <brian@projectweekend.net>
2018-04-21 09:12:55 -0500

    Build the whole thing in function

--- aa/app.py
+++ aa/app.py
@@ -9,14 +9,13 @@ from aa.web_api import UnitInfoResource
 def create_application():
     cors = CORS(allow_all_origins=True, allow_all_headers=True,
                 allow_all_methods=True, allow_credentials_all_origins=True)
-    return falcon.API(middleware=[cors.middleware])
+    app = falcon.API(middleware=[cors.middleware])
+    unit_info_resource = UnitInfoResource()
+    simulation_resource = SimulationResource(army_builder=build_army,
+                                             battle_simulator=simulate)
+    app.add_route('/unit-info', unit_info_resource)
+    app.add_route('/', simulation_resource)
+    return app
 
 
 application = create_application()
-
-unit_info_resource = UnitInfoResource()
-simulation_resource = SimulationResource(army_builder=build_army,
-                                         battle_simulator=simulate)
-
-application.add_route('/unit-info', unit_info_resource)
-application.add_route('/', simulation_resource)
